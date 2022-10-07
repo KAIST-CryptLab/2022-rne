cce83b9a4797d8f2383092cfd5a009fd7712f69a
Jihun Park <oxjihun9@gmail.com>
2022-10-07 18:57:00 +0900

    Make minor changes to remove errors

--- brute-force/main.cpp
+++ brute-force/main.cpp
@@ -1,107 +1,19 @@
-<<<<<<< HEAD
+// https://blockdmask.tistory.com/338
+// https://cplusplus.com/reference/string/string/insert/
+
 #include <iostream>
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
-#include <process.h>
 #include <fstream>
 using namespace std;
 int n, k, m, r, t;
-int f(int i);
+string f(int i)
 {
     string ans = "";
     while(i > 0)
     {
-        ans = strcat((char)(i % 2), ans);
-        i = i / 2;
-    }
-    return ans;
-}
-int main()
-{
-    char cmd[1000];
-    cout << "numofboxes: ";
-    cin >> m;
-    cout << "blocksize: ";
-    cin >> n;
-    cout << "keysize: ";
-    cin >> k;
-    cout << "rounds: ";
-    cin >> r;
-    cout << "testcase: ";
-    cin >> t;
-    int M[200][200] = {0,}, C[200][200] = {0,};
-    for(int j = 0; j < t; j++)
-    {
-        cout << "message" << j + 1 << ": ";
-        for(int i = n - 1; i >= 0; i--)
-        {
-            scanf("%1d", &M[j][i]);
-        }
-        cout << "ciphertext" << j + 1 << ": ";
-        for(int i = n - 1; i >= 0; i--)
-        {
-            scanf("%1d", &C[j][i]);
-        }
-    }
-    time_t start = time(NULL);
-    int j = 1, k=0;
-    string cip, mes = "";
-    for(int i = 1; i <= n; i++)
-    {
-        j *= 2;
-    }
-    for(int i = 0; i < j; i++)
-    {
-        k = 0;
-        for(int me = 0; me < t; me++)
-        {
-            mes = "";
-            for(int y = 0; y < m; y++)
-            {
-                mes = strcat(mes, (char)(M[me][y]));
-            }
-            cip = "";
-            for(int y = 0; y < m; y++)
-            {
-                cip = strcat(cip, (char)(C[me][y]));
-            }
-            sprintf(cmd, "c:\\Users\\user\\Desktop\\RnE\\ANFConv\\.cpp %d %s", mes, f(i));
-            system(cmd);
-            ifstream input(".txt");
-            string cipher = "";
-            input >> cipher;
-            if(cipher != cip)
-            {
-                me = t;
-                k += 1;
-            }
-            if(k == t)
-            {
-                print("%s\n", f(i));
-                break;
-            }
-        }
-    }
-    time_t end1 = time(NULL);
-    printf("�ҿ�ð�: %lfmms\n", (double)(end1 - start));
-    return 0;
-}
-=======
-#include <iostream>
-#include <stdio.h>
-#include <time.h>
-#include <string.h>
-#include <process.h>
-#include <fstream>
-using namespace std;
-int n, k, m, r, t;
-int f(int i);
-{
-    string ans = "";
-    while(i > 0)
-    {
-        ans = strcat((char)(i % 2), ans);
+        ans.insert(0, 1, i%2 + 48);
         i = i / 2;
     }
     return ans;
@@ -150,12 +62,12 @@ int main()
             mes = "";
             for(int y = 0; y < m; y++)
             {
-                mes = strcat(mes, (char)(M[me][y]));
+                mes.push_back((char)(M[me][y]));
             }
             cip = "";
             for(int y = 0; y < m; y++)
             {
-                cip = strcat(cip, (char)(C[me][y]));
+                cip.push_back((char)(C[me][y]));
             }
             sprintf(cmd, "{ echo %s; echo %s; } | ../lowmc/LowMC > ciphertext.txt", f(i), mes);
             system(cmd);
@@ -169,13 +81,13 @@ int main()
             }
             if(k == t)
             {
-                print("%s\n", f(i));
+                printf("%s\n", f(i));
                 break;
             }
         }
     }
     time_t end1 = time(NULL);
     printf("소요시간: %lfmms\n", (double)(end1 - start));
+    /**/
     return 0;
 }
->>>>>>> Add brute-force cpp code Yuseong sent me via KakaoTalk
